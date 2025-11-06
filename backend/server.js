bebf0cbe73016f7e6854c9128c98ca91bd81fc59
nathira99 <nathirafarveen99@gmail.com>
2025-11-06 20:49:21 +0530

    resolved

--- backend/server.js
+++ backend/server.js
@@ -26,4 +26,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+if (process.env.NODE_ENV !== "production") {
+  
+  app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT}`));
+}
+
+module.exports = app; 
