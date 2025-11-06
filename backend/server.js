7a690fc70d797dd58601d9b54db3260b840eaa4c
nathira99 <nathirafarveen99@gmail.com>
2025-11-06 20:38:23 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const cors = require("cors");
 
 app.use(cors({
-  origin: ["https://password-reset-system-fe.netlify.app/"], // your frontend domain
+  origin: ["https://password-reset-system-fe.netlify.app"], // your frontend domain
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
